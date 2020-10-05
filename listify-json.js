fb80f72b445657dde3b314702bc483731296cf5b
edisionnano <samantas5855@gmail.com>
2020-10-05 05:16:11 +0300

    Add files via upload

---
+++ listify-json.js
@@ -0,0 +1,24 @@
+
+$.ajax({
+    url: 'https://api.github.com/repos/pineappleEA/pineappleEA.github.io/releases',
+    type: "get",
+    dataType: "json",
+   
+    success: function(data) {
+        drawTable(data);
+    }
+});
+
+function drawTable(data) {
+	drawRow(data[0]);
+    for (var i = 2; i < data.length - 7; i++) {
+        drawRow(data[i]);
+    }
+}
+
+function drawRow(rowData) {
+    var row = $("<tr />")
+    $("#personDataTable").append(row); 
+    row.append($('<div class="featured"><a href=' + rowData.assets[0].browser_download_url + ">" + rowData.tag_name + "</a></div>"));
+}
+
